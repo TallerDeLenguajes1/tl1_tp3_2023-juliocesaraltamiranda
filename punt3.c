c87743427658dd1b5e80c7d326ca393648fb4f24
AltamirandaJulio22 <102418766+AltamirandaJulio22@users.noreply.github.com>
2023-05-06 21:27:45 -0300

    punto 5 terminado

--- punt3.c
+++ punt3.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define Cinconombres 5
 #define MaxCadena 100
 
 
 char *cargarNombre(char *buff, int itera);
-void cargarNombres(char **puntNombres, char *buff);
-void mostrarNombres(char **puntNombres);
-void liberarMemoria(char **puntNombres, char *buff);
+void cargarNombres(char **puntNombres, char *buff, int n);
+void mostrarNombres(char **puntNombres, int n);
+void liberarMemoria(char **puntNombres, char *buff, int n);
 
 int main() {
 
-    char **puntNombres = (char **)malloc(sizeof(char *) *Cinconombres);
+    int n;
+    printf("Ingrese el numero de nombres que ingresara: ");
+    scanf("%d", &n);
+    printf("\n");
+
+    char **puntNombres = (char **)malloc(sizeof(char *) *n);
     char *buff = malloc(sizeof(char)*MaxCadena);
 
-    cargarNombres(puntNombres, buff);
-    mostrarNombres(puntNombres);
-    liberarMemoria(puntNombres, buff);
+    cargarNombres(puntNombres, buff, n);
+    mostrarNombres(puntNombres, n);
+    liberarMemoria(puntNombres, buff, n);
 
     return 0;
 }
@@ -38,24 +42,24 @@ char *cargarNombre(char *buff, int itera) {
     return nombre;
 }
 
-void cargarNombres(char **puntNombres, char *buff) {
-    
-    for(int i = 0; i < Cinconombres; i++) {
+void cargarNombres(char **puntNombres, char *buff, int n) {
+    getchar();
+    for(int i = 0; i < n; i++) {
         puntNombres[i] = cargarNombre(buff, i);
         printf("\n");
     }
 }
 
-void mostrarNombres(char **puntNombres) {
+void mostrarNombres(char **puntNombres, int n) {
 
-    for(int i = 0; i < Cinconombres; i++) {
+    for(int i = 0; i < n; i++) {
         printf("Nombre - %d: %s \n", i+1, puntNombres[i]);
     }
 }
 
-void liberarMemoria(char **puntNombres, char *buff) {
+void liberarMemoria(char **puntNombres, char *buff, int n) {
 
-    for(int i = 0; i < Cinconombres; i++) {
+    for(int i = 0; i < n; i++) {
         free(puntNombres[i]);
     }
     
